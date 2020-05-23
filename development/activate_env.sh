561cea53c71a7979e98206affaf87decff35b234
Leonardo Seccia <leo.seccia@lexisnexis.co.uk>
2020-05-23 11:12:02 +0100

    activate shortcut

---
+++ development/activate_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# __usage() {
+#     echo "Usage: ${BASH_SOURCE[0]##*/} environment name | optional, default='venv'" 1>&2;
+#     exit 1;
+# }
+
+#location of repos
+. ${@:-venv}/bin/activate
