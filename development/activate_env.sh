340fd4e1ccc3694dd2824f7babec458b1ba50861
Leonardo Seccia <leo.seccia@lexisnexis.co.uk>
2020-05-23 11:13:37 +0100

    comment

--- development/activate_env.sh
+++ development/activate_env.sh
@@ -5,5 +5,8 @@
 #     exit 1;
 # }
 
+# to make this work I have added an alias to it in my .bashrc ie.
+# alias a=". ~/development/linux-crystals/development/activate_env.sh"
+
 #location of repos
 . ${@:-venv}/bin/activate
